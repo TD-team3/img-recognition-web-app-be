c106353828f79d6bb6369ef66b3f1bab91512e0d
aldotele <aldo.telese@hotmail.it>
2020-12-29 19:16:04 +0100

    added upload view

--- upload_app/views.py
+++ upload_app/views.py
@@ -1,34 +1,34 @@
-from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage  # added
-from django.http import HttpResponse
-from django.core.files.temp import NamedTemporaryFile
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+import json
+from authentication_app.base_auth_classes.authentication import auth
+from .base_upload_classes.upload import ImageUploader
+from .base_upload_classes.recognition import ImageRecognizer
 
 
 def upload(request):
-    if request.method == 'POST':
-        recogn_dict = {}
-        list_of_files = []  # creating an empty list where all uploaded files will go
-        print(request.FILES)
-        for file in request.FILES.getlist('myfile'):
-            img_temp = NamedTemporaryFile()
-            img_temp.write(file.read())
-            img_temp.flush()
-            print(img_temp.name)
-            print(img_temp.tell())
+    # getting the json data from the http POST request made by front-end
+    json_str = request.POST.get('data', '')  # FIRST PARAMETER MAY CHANGE !!!
+    if json_str:
+        # converting the json data into a json object
+        json_obj = json.loads(json_str)
+        if 'username' in json_obj and 'token' in json_obj:
+            username = json_obj['username']
+            token = json_obj['token']
+            if request.method == 'POST' and auth.is_token_valid(username, token):
+                my_image_uploader = ImageUploader()
+                my_image_uploader.process_images(request.FILES, 'myfile')  # SECOND PARAMETER MAY CHANGE !!!
+                my_image_recognizer = ImageRecognizer()
+                my_image_recognizer.recognize_images(my_image_uploader)
 
-            list_of_files.append(img_temp)  # per passare alla recognition
+                final_result = my_image_recognizer.recognition_dictionary
+                # WRITE JSON BASED ON FINAL RESULT !!!
 
-            # recognized = recognition_function(img_temp)
-            recogn_dict[file.name] = recognized
+                return HttpResponse('PUT JSON HERE', status=200)
+            else:
+                return HttpResponseForbidden()
+        else:
+            return HttpResponseBadRequest()
+    else:
+        return HttpResponseBadRequest()
 
-
-        #for f in request.FILES.getlist('myfile'):  # where myFile is the name= of the HTML tag
-            #filename = f.name
-            #list_of_files.append(filename)
-
-
-    return HttpResponse('Ok', status=200)
-
-
-        # BELOW WE ACCESS THE DICTIONARY
-        # THE KEY OF THE DICTIONARY WILL BE THE NAME= OF THE INPUT TAG IN THE HTML FORM (e.g. "myFile")
+# note that 'myfile' is the name= attribute of the form/input TAG in the HTML, and may change
