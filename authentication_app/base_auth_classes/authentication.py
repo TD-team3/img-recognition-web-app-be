ba44769f82d24e3a08323e434c34b501d6566766
aldotele <aldo.telese@hotmail.it>
2021-01-03 15:46:11 +0100

    bug fix 2 username case insensitive

--- authentication_app/base_auth_classes/authentication.py
+++ authentication_app/base_auth_classes/authentication.py
@@ -14,7 +14,6 @@ class Authentication:
         self.token = {}
 
     def is_auth_data_valid(self, username, password):
-        username = username.lower()
         if username == Authentication.USERNAME and password == Authentication.PASSWORD:
             token_str = Token.generate_token(username, password)
             # generated token string gets saved as value of the username key of the token attribute
@@ -24,7 +23,6 @@ class Authentication:
             return False
 
     def is_token_valid(self, username, token_str):
-        username = username.lower()
         if username in self.token:
             if token_str == self.token[username]:
                 if not Token.is_token_expired(token_str):
