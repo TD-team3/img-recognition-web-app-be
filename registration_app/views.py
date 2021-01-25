0390555d9eba6865a2cba3b0d2214854886f6472
aldotele <aldo.telese@hotmail.it>
2021-01-25 15:29:10 +0100

    removal of password double check

--- registration_app/views.py
+++ registration_app/views.py
@@ -9,13 +9,11 @@ def signup(request):
         json_str = request.POST.get('data', '')
         if json_str:
             json_obj = json.loads(json_str)
-            if 'mail' in json_obj and 'name' in json_obj and 'surname' in json_obj and 'password' in json_obj and\
-                    'password2' in json_obj:
+            if 'mail' in json_obj and 'name' in json_obj and 'surname' in json_obj and 'password' in json_obj:
                 mail = json_obj['mail'].lower()
                 name = json_obj['name']
                 surname = json_obj['surname']
                 password = json_obj['password']
-                password2 = json_obj['password2']
 
                 # checking if email already exists in database
                 check_mail = Users.objects.filter(mail=mail)
@@ -23,12 +21,7 @@ def signup(request):
                 if check_mail.count() != 0:
                     res = json.dumps('email already registered.')
                     return HttpResponseNotAllowed(res, content_type='application/json')
-                # validating two passwords
-                if password != password2:
-                    res = json.dumps('passwords do not match.')
-                    return HttpResponseNotAllowed(res, content_type='application/json')
 
-                # if no problem is found, a user gets created
                 user = Users(
                     mail=mail,
                     name=name,
