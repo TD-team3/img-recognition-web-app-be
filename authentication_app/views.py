66aaba290ad7dd2a371f718f70f4e3b1d6d59fae
aldotele <aldo.telese@hotmail.it>
2021-02-18 15:54:00 +0100

    refactoring JWT Authentication and DB methods

--- authentication_app/views.py
+++ authentication_app/views.py
@@ -1,4 +1,5 @@
-from authentication_app.base_auth_classes.authentication import auth
+from authentication_app.base_auth_classes.authentication import TokenJwt
+from registration_app.views import Users
 from django.http import HttpResponse
 import json
 
@@ -13,11 +14,13 @@ def login(request):
             username = json_obj['username'].lower()
             password = json_obj['password']
             # the following checks if username is present in database and if password is correct
-            if auth.is_auth_data_valid(username, password):
-                # the following creates a token based on username and current time
-                encoded_jwt = auth.generate_and_save_jwt(username)
+            if Users.is_auth_data_valid(username, password):
+                # creates and saves a JWT token based on username and current time
+                encoded_jwt = TokenJwt.generate_and_save_jwt(username)
+                # builds json with jwt inside it
+                json_str = json.dumps({'token': str(encoded_jwt)})
 
-                return HttpResponse(encoded_jwt, status=200, content_type='application/json')
+                return HttpResponse(json_str, status=200, content_type='application/json')
             else:
                 return HttpResponse('authentication not valid', status=401)
 
